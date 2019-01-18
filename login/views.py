52130aca02f87694cfb6a0ab274db18a0ff3d703
yuxichen <16307130202@fudan.edu.cn>
2019-01-18 17:57:20 +0800

    yuxichen_branch

--- login/views.py
+++ login/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
+def index(request):
+    pass
+    return render(request, 'mytest/index.html')
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username'   , None)
+        password = request.POST.get('password', None)
+        message = "所有字段都必须填写！"
+        if username and password:
+            username = username.strip()
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('mytest/index.html')
+                else:
+                    message = "密码不正确！"
+            except:
+                message = "用户名不存在！"
+        return render(request, 'login.html', {"message": message})
+    return render(request, 'login.html')
+
+
+def logout(request):
+    pass
+    return redirect('mytest/index.html')
